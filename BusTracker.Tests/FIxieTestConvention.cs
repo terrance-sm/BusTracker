6c2cb769956d171793c60659d47cd30c9e29bf6a
Terrance <terrance.smithy@gmail.com>
2015-12-01 15:06:48 -0600

    Added Basic Fixie Test

---
+++ BusTracker.Tests/FIxieTestConvention.cs
@@ -0,0 +1,20 @@
+﻿using Fixie;
+
+namespace BusTracker.Tests
+{
+    public class FIxieTestConvention: Convention
+    {
+        public FIxieTestConvention()
+        {
+            Classes.NameEndsWith("Tests");
+
+            Methods.Where(method => method.IsPublic || method.IsAsync());
+
+            ClassExecution.CreateInstancePerClass().ShuffleCases();
+
+            //CaseExecution Can Added the Skip Stuff here
+
+            //Parameters()
+        }
+    }
+}
